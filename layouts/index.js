7e5afb5c04c50f364a621b735d04bfe339216dbc
Anestis Finstad <munkeyten@protonmail.com>
2020-06-21 23:08:03 +0200

    Removed GA
    testing

--- layouts/index.js
+++ layouts/index.js
@@ -3,15 +3,8 @@ const DefaultLayout = ({ children }) => (
       <header>
      
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170228462-1"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)}
-    gtag('js', new Date());
 
-    gtag('config', 'UA-170228462-1');
-    </script>
-
-        <h1>Next.js & Prismic.io Blog</h1>
+        <h1>Yum Tash </h1>
       </header>
       <main>{children}</main>
     </div>
