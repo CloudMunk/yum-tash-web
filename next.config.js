6beae85bc5822f2833e5aadfec44c582f46bc20a
Anestis Finstad <munkeyten@protonmail.com>
2020-06-21 21:24:26 +0200

    Google analytics
    setting up GA

---
+++ next.config.js
@@ -0,0 +1,20 @@
+
+  const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+        "google-analytics": {
+            "clientId": "687590004878-m6e54ara1jdquhhs73i35nlg2a8qpee8.apps.googleusercontent.com",
+            "views": "ga:221720030"
+        }
+    }
+  }
+
+  return {
+    "google-analytics": {
+        "clientId": "687590004878-m6e54ara1jdquhhs73i35nlg2a8qpee8.apps.googleusercontent.com",
+        "views": "ga:221720030"
+    }
+  }
+}
