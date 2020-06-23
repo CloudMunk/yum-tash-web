3411a6e231ab2d20a62c3699fe0f2c4bb70dceea
Anestis Finstad <munkeyten@protonmail.com>
2020-06-23 23:33:03 +0200

    Removed letter
    Removed letter

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1fdc95dd9ea5fe4e8dec";
+/******/ 	var hotCurrentHash = "5d6f49ecc82b4c3f9cef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ const Index = (props) => {
                   lineHeight: '1.6em'
                 }}
               >
-                A food blog with fresh, zesty recipes. to
+                A food blog with fresh, zesty recipes.
               </h3>
             </div>
             {/* Card */}
