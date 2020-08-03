33f0f52927f7fde7a6361234ae9df2ea57883821
Anestis Finstad <munkeyten@protonmail.com>
2020-08-03 10:23:08 +0200

    Menu updates

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fa6463936bac1f5b371f";
+/******/ 	var hotCurrentHash = "2e23854519420f42a8a6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
