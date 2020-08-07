980d523f95ac520ee5aa575e983e68e355a15205
Anestis Finstad <munkeyten@protonmail.com>
2020-08-07 13:44:44 +0200

    Double logo

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "302ebe695f4b01a7bcc9";
+/******/ 	var hotCurrentHash = "1961e2162707fa24103c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
