8056fe4a1f1dd377faa8f4e53151e638faa835f3
Anestis Finstad <munkeyten@protonmail.com>
2020-08-10 17:16:32 +0200

    Mobile layout landing
    Mobile layout landing page coming along

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1c5a796a439f31b70fbd";
+/******/ 	var hotCurrentHash = "d4792cf49ad188b42fc4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
